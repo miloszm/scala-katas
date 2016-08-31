0142ad3fd39aca3f41db533716b4dbff8408746c
muszynskim <MuszynskiM@quindell.com>
2016-08-31 14:14:35 +0100

    FizzBuzz

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "scala-katas"
+
+version := "1.0"
+
+scalaVersion := "2.11.4"
+
+isSnapshot := true
+
+libraryDependencies += "org.specs2" %% "specs2" % "2.3.12"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+
---
+++ src/main/scala/com/mm/FizzBuzz.scala
@@ -0,0 +1,36 @@
+package com.mm
+
+/**
+  * 31/08/2016
+  */
+object FizzBuzz {
+  def generate2(i: Int):String = {
+    def fizzBuzz(n:Int, s:String)(i:Int):Option[String] =
+      if ((i % n == 0) || i.toString.contains(n.toString)) Some(s) else None
+    val fizz = fizzBuzz(3,"Fizz")(_)
+    val buzz = fizzBuzz(5,"Buzz")(_)
+    val list = for {
+      Some(fb) <- List(fizz(i),buzz(i))
+    } yield fb
+    if (list.isEmpty) i.toString else list.mkString
+  }
+
+  def generate3(i: Int):String = {
+    def fizzBuzz(n:Int, s:String)(i:Int):Option[String] =
+      if ((i % n == 0) || i.toString.contains(n.toString)) Some(s) else None
+    val fizz = fizzBuzz(3,"Fizz")(_)
+    val buzz = fizzBuzz(5,"Buzz")(_)
+    val list = List(fizz,buzz).map(_(i)).collect{case(Some(s)) => s}
+    if (list.isEmpty) i.toString else list.mkString
+  }
+
+  def generate(i: Int):String = {
+    def fizzBuzz(n:Int, s:String)(i:Int):Option[String] =
+      if ((i % n == 0) || i.toString.contains(n.toString)) Some(s) else None
+    val fizz = fizzBuzz(3,"Fizz")(_)
+    val buzz = fizzBuzz(5,"Buzz")(_)
+    val list = fizz(i).toList ::: buzz(i).toList
+    if (list.isEmpty) i.toString else list.mkString
+  }
+
+}
