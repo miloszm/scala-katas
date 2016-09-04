39f02caf01f0574cddefdb8d29023e4fb0f0d7e4
muszynskim <MuszynskiM@quindell.com>
2016-09-04 16:47:18 +0100

    fizzbuzz 3 improved 2

--- src/main/scala/com/ce/FizzBuzzStep3.scala
+++ src/main/scala/com/ce/FizzBuzzStep3.scala
@@ -6,29 +6,24 @@ object FizzBuzzStep3 {
 
   object OutputType extends Enumeration {
     type OutputType = Value
-    val Fizz, Buzz, FizzBuzz, Lucky, Integer = Value
+    val fizz, buzz, fizzbuzz, lucky, integer = Value
   }
 
   def generate(i: Int):(String,OutputType) = {
     val fizzList = List("fizz").filter(_ => i % 3 == 0)
     val buzzList = List("buzz").filter(_ => i % 5 == 0)
-    val fizzBuzzOutputType = (fizzList, buzzList) match {
-      case (f::Nil, b::Nil) => OutputType.FizzBuzz
-      case (f::Nil, Nil) => OutputType.Fizz
-      case (Nil, b::Nil) => OutputType.Buzz
-      case _ => OutputType.Buzz
-    }
+    def lucky(n:Int) = Option(("lucky",OutputType.lucky)).filter(_ => n.toString contains '3')
     val fizzBuzzList = fizzList ::: buzzList
-    val fizzBuzz = Option((fizzBuzzList.mkString,fizzBuzzOutputType)).filterNot(_ => fizzBuzzList.isEmpty)
-    def lucky(n:Int) = Option(("lucky",OutputType.Lucky)).filter(_ => n.toString contains '3')
+    val fizzBuzzString = fizzBuzzList.mkString
+    val fizzBuzz = if (fizzBuzzList.isEmpty) None else Some (fizzBuzzString,OutputType.withName(fizzBuzzString))
     val fizzBuzzLucky = lucky(i).orElse(fizzBuzz)
-    fizzBuzzLucky.getOrElse((i.toString,OutputType.Integer))
+    fizzBuzzLucky.getOrElse((i.toString,OutputType.integer))
   }
 
   def generate(r:Range):String = {
     val counter = Array.fill[Int](OutputType.maxId)(0)
     val mainOutput = r.map(generate(_)).map(o => {counter(o._2.id)+=1; o._1}).mkString(" ")
-    val statOutput = (0 until OutputType.maxId).map(i => OutputType(i).toString.toLowerCase + ": " + counter(i)).mkString("\n")
+    val statOutput = (0 until OutputType.maxId).map(i => OutputType(i).toString + ": " + counter(i)).mkString("\n")
     mainOutput + "\n" + statOutput
   }
 
