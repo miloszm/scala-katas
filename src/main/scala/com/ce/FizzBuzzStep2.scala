374348f81664d6c4e7ad671faee886d13cb4b72c
muszynskim <MuszynskiM@quindell.com>
2016-09-04 16:58:32 +0100

    fizzbuzz 3 improved 4

--- src/main/scala/com/ce/FizzBuzzStep2.scala
+++ src/main/scala/com/ce/FizzBuzzStep2.scala
@@ -4,7 +4,7 @@ object FizzBuzzStep2 {
 
   def generate(i: Int):String = {
     val fizzBuzzList= List("fizz").filter(_ => i % 3 == 0) ::: List("buzz").filter(_ => i % 5 == 0)
-    val fizzBuzz = Some(fizzBuzzList.mkString).filterNot(_ => fizzBuzzList.isEmpty)
+    val fizzBuzz = Option(fizzBuzzList.mkString).filterNot(_ => fizzBuzzList.isEmpty)
     def lucky(n:Int) = Option("lucky").filter(_ => n.toString contains '3')
     val fizzBuzzLucky = lucky(i).orElse(fizzBuzz)
     fizzBuzzLucky.getOrElse(i.toString)
